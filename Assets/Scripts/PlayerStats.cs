300496482fa68f9a476004f720279889c0e43a99
Hussbo10 <husseinalimahdi896@gmail.com>
2026-03-05 21:09:00 -0700

    StatBoosts and Item Destroy
    Items now give stat boosts on contacted and are destroyed upon contact

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public float Health;
+    public float MaxHealth;
+    public float Damage;
+    public float Speed;
+    public float MaxSpeed;
+    public float Mana;
+    public float MaxMana;
+
+    private void Start()
+    {
+        Health = MaxHealth;
+        Speed = MaxSpeed;
+        Mana = MaxMana;
+
+    }
+}
